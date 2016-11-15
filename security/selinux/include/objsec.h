201e092fc89552433d26586b82ffd9852d07f6ca
Andreas Gruenbacher <agruenba@redhat.com>
2016-11-15 11:06:40 +0100

    selinux: Convert isec->lock into a spinlock
    commit 9287aed2ad1ff1bde5eb190bcd6dccd5f1cf47d3 upstream.
    
    Convert isec->lock from a mutex into a spinlock.  Instead of holding
    the lock while sleeping in inode_doinit_with_dentry, set
    isec->initialized to LABEL_PENDING and release the lock.  Then, when
    the sid has been determined, re-acquire the lock.  If isec->initialized
    is still set to LABEL_PENDING, set isec->sid; otherwise, the sid has
    been set by another task (LABEL_INITIALIZED) or invalidated
    (LABEL_INVALID) in the meantime.
    
    This fixes a deadlock on gfs2 where
    
     * one task is in inode_doinit_with_dentry -> gfs2_getxattr, holds
       isec->lock, and tries to acquire the inode's glock, and
    
     * another task is in do_xmote -> inode_go_inval ->
       selinux_inode_invalidate_secctx, holds the inode's glock, and
       tries to acquire isec->lock.
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    [PM: minor tweaks to keep checkpatch.pl happy]
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: Alexander Grund <theflamefire89@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- security/selinux/include/objsec.h
+++ security/selinux/include/objsec.h
@@ -43,7 +43,8 @@ struct task_security_struct {
 
 enum label_initialized {
 	LABEL_INVALID,		/* invalid or not initialized */
-	LABEL_INITIALIZED	/* initialized */
+	LABEL_INITIALIZED,	/* initialized */
+	LABEL_PENDING
 };
 
 struct inode_security_struct {
@@ -59,6 +60,7 @@ struct inode_security_struct {
 	u32 tag;		/* Per-File-Encryption tag */
 	void *pfk_data; /* Per-File-Key data from ecryptfs */
 	struct mutex lock;
+	spinlock_t lock;
 };
 
 struct file_security_struct {
